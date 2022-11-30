9fe612d01fabac9b39fbeccbcff0e50193da5a4f
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-30 11:02:23 -0300

    Create operate function

--- function.js
+++ function.js
@@ -4,3 +4,7 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const basicOperations = {add, substract, multiply, divide};
+
+function operate (operator, a, b) {
+    return basicOperations[operator](a, b);
+}

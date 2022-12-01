7a0faa69b1c09d5aeb26c17f83ccba0123596b1a
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-30 22:59:18 -0300

    Advance in getting full funcionality
    After a lot of time I was able to resolve most of the challenges,
    although now I'm starting to see a more clearer picture and I think
    that I'll be able to improve the code. I was so deep in thought that
    I completely forgot about committing often, so that's one thing to
    have in mind in the next projects. Avoid this leaps from almost
    nothing to almost everything.

--- function.js
+++ function.js
@@ -4,7 +4,74 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const basicOperations = {add, substract, multiply, divide};
+const screen = document.querySelector('.visor');
+const btns = document.querySelectorAll('.btn');
+let data = new Object();
 
 function operate (operator, a, b) {
+    a = Number(a);
+    b = Number(b);
     return basicOperations[operator](a, b);
 }
+
+function reset() {
+    data = {first: "", second: "", operator: "", screen: 0, stage: 0};
+}
+
+function display() {
+    screen.textContent = data.screen;
+}
+
+reset(0);
+    
+btns.forEach(item => item.addEventListener('click', () => {
+        
+    if(! data.stage) {
+        if(item.classList[1] === 'pad') {
+            data.first += item.id;
+            data.screen = data.first;
+            display();
+        }
+        else if (item.classList[1] === 'op') {            
+            data.operator = item.id;
+            console.log(data.operator);
+            data.stage++;
+        }
+        else if(item.classList[1] === 'ac') {
+            reset();
+            display();
+        }
+    }
+
+    else if (data.stage === 1){
+        if(item.classList[1] === 'pad') {
+            data.second += item.id;
+            data.screen = data.second;
+            display();
+        }
+        else if (item.classList[1] === 'equal'){
+            data.screen = operate(data.operator, data.first, data.second);
+            display();
+            data.operator = "";
+            data.first = data.screen;
+            data.second = "";
+        }
+        else if(item.classList[1] === 'ac') {
+            reset();
+            display();
+        }
+        else if (item.classList[1] === 'op' && data.second) {
+            data.screen = operate(data.operator, data.first, data.second);
+            display();
+            data.operator = item.id;
+            console.log(data.operator);
+            data.first = data.screen;
+            data.second = "";
+        }
+        
+    }
+    else if(item.classList[1] === 'ac') {
+        reset();
+        display();
+    }
+}));

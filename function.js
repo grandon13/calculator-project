06b9c6db91d9e6e35e6a74644d96606877a42697
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-30 23:51:58 -0300

    Go back to start over
    HTML and CSS are the same, but this is an attempt to improve the
    javaScript. I think that after a first try I have a deeper understanding
    and could do much better. We'll see.

--- function.js
+++ function.js
@@ -1,12 +1,9 @@
-const add = (a, b) => a + b;
-const substract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-const basicOperations = {add, substract, multiply, divide};
-const screen = document.querySelector('.visor');
-const btns = document.querySelectorAll('.btn');
-let data = new Object();
+const basicOperations = {
+    add: (a, b) => a + b,
+    substract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+};
 
 function operate (operator, a, b) {
     a = Number(a);
@@ -14,64 +11,18 @@ function operate (operator, a, b) {
     return basicOperations[operator](a, b);
 }
 
+let data = new Object();
 function reset() {
-    data = {first: "", second: "", operator: "", screen: 0, stage: 0};
+    data = {first: "", second: "", operator: "", screen: 0};
 }
+reset();
 
+const screen = document.querySelector('.visor');
 function display() {
     screen.textContent = data.screen;
 }
 
-reset(0);
-    
-btns.forEach(item => item.addEventListener('click', () => {
+const btns = document.querySelectorAll('.btn');    
+btns.forEach( item => item.addEventListener( 'click', () => {
         
-    if(! data.stage) {
-        if(item.classList[1] === 'pad') {
-            data.first += item.id;
-            data.screen = data.first;
-            display();
-        }
-        else if (item.classList[1] === 'op') {            
-            data.operator = item.id;
-            console.log(data.operator);
-            data.stage++;
-        }
-        else if(item.classList[1] === 'ac') {
-            reset();
-            display();
-        }
-    }
-
-    else if (data.stage === 1){
-        if(item.classList[1] === 'pad') {
-            data.second += item.id;
-            data.screen = data.second;
-            display();
-        }
-        else if (item.classList[1] === 'equal'){
-            data.screen = operate(data.operator, data.first, data.second);
-            display();
-            data.operator = "";
-            data.first = data.screen;
-            data.second = "";
-        }
-        else if(item.classList[1] === 'ac') {
-            reset();
-            display();
-        }
-        else if (item.classList[1] === 'op' && data.second) {
-            data.screen = operate(data.operator, data.first, data.second);
-            display();
-            data.operator = item.id;
-            console.log(data.operator);
-            data.first = data.screen;
-            data.second = "";
-        }
-        
-    }
-    else if(item.classList[1] === 'ac') {
-        reset();
-        display();
-    }
 }));

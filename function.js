68238a90371574e4d36588e031eca956f3e9ef4d
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-12-01 13:14:24 -0300

    Finish the project with a 100%
    This version tackles every function that was asked and a little more.
    I added functionality for negative numbers too.

--- function.js
+++ function.js
@@ -1,3 +1,5 @@
+let data = {first: "", second: "", operator: ""};
+
 const basicOperations = {
     add: (a, b) => a + b,
     substract: (a, b) => a - b,
@@ -8,9 +10,9 @@ const basicOperations = {
 function operate (operator, a, b) {
     a = Number(a);
     b = Number(b);
-    
+
     let total = basicOperations[operator](a, b);
-    let magnitude = Math.floor(Math.log10(total));
+    let magnitude = total !== 0 ? Math.floor(Math.log10(Math.abs(total))) : 0;    
     let decimals = 9 - magnitude;
 
     if (magnitude > 9) {
@@ -21,42 +23,66 @@ function operate (operator, a, b) {
     return total;
 }
 
-let data = {first: "", second: "", operator: ""};
-
 const screen = document.querySelector('.visor');
 function display(a) {
-    a = Number(a);
-    screen.textContent = a;
+    a = a.toString();
+    if(Number(a) === 0 && a.indexOf('.') < 0 && a.indexOf('-') < 0) {
+        a = Number(a);
+    }        
+        screen.textContent = a;
 }
 
+let firstZero = false;
 const btns = document.querySelectorAll('.btn');    
 btns.forEach( item => item.addEventListener( 'click', () => {
 
     if (item.classList[1] === 'ac') {
         data = {first: "", second: "", operator: ""}
-        display(0);
+        display('0');
     }
 
     if (!data.operator) {
         if (item.classList[1] === 'del') {
-            data.first = data.first.slice(0, data.first.length -1);
+            data.first = data.first.slice(0, data.first.length - 1);
             display(data.first);
         }
 
         if (item.classList[1] === 'pad' && data.first.length < 10) {
             data.second = "";
-            data.first += item.id;
+
+            if (!firstZero) {             
+                data.first += item.id;
+                display(data.first);
+                firstZero = data.first === '0';
+            }
+            else {
+                data.first = item.id;
+                display(data.first);
+                firstZero = data.first === '0';
+            }
+        }
+
+        else if (!data.first && item.id === 'substract') {
+            data.first = '-';
             display(data.first);
+            firstZero = false;
         }
 
         else if (data.first && item.classList[1] === 'op') {
             data.operator = item.id;
+            firstZero = false;
         }
 
         else if (data.second && item.classList[1] === 'op') {
             data.first = data.second;
             data.second = "";
             data.operator = item.id;
+            firstZero = false;
+        }
+        else if( data.first.length > 0 && data.first.indexOf('.') < 0 && item.classList[1] === 'dot') {
+            data.first += item.id;
+            display(data.first);
+            firstZero = false;
         }
     }
 
@@ -67,8 +93,22 @@ btns.forEach( item => item.addEventListener( 'click', () => {
         }
 
         if (item.classList[1] === 'pad' && data.second.length < 10) {
-            data.second += item.id;
+            if (!firstZero) {             
+                data.second += item.id;
+                display(data.second);
+                firstZero = data.second === '0';
+            }
+            else {
+                data.second = item.id;
+                display(data.second);
+                firstZero = data.second === '0';
+            }
+        }
+
+        else if (!data.second && item.id === 'substract') {
+            data.second = '-';
             display(data.second);
+            firstZero = false;
         }
 
         else if (data.second && item.classList[1] === 'op') {
@@ -76,6 +116,7 @@ btns.forEach( item => item.addEventListener( 'click', () => {
             display(data.first);
             data.second = "";
             data.operator = item.id;
+            firstZero = false;
         }
 
         else if (data.second && item.classList[1] === 'equal') {
@@ -83,6 +124,13 @@ btns.forEach( item => item.addEventListener( 'click', () => {
             display(data.second);
             data.first = "";
             data.operator = "";
-        }        
+            firstZero = false;
+        }
+
+        else if( data.second.length > 0 && data.second.indexOf('.') < 0 && item.classList[1] === 'dot') {
+            data.second += item.id;
+            display(data.second);
+            firstZero = false;
+        }
     }
 }));
